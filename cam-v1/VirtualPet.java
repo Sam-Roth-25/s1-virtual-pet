44e20d9be08e78111da27d737350c5b70eea42eb
Sam-Roth-25 <Sam-Roth-25@ahs.local>
2023-10-04 15:04:56 -0700

    save

--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -6,7 +6,8 @@
 public class VirtualPet {
     
     VirtualPetFace face;
-    int hunger = 0;   // how hungry the pet is.
+    private int hunger = 0;   // how hungry the pet is.
+    private int tiredness=0;
     
     // constructor
     public VirtualPet() {
@@ -27,8 +28,15 @@ public class VirtualPet {
     
     public void exercise() {
         hunger = hunger + 3;
-        face.setMessage("1, 2, 3, jump.  Whew.");
-        face.setImage("tired");
+        tiredness++;
+        if (tiredness<4){
+            face.setMessage("That was great");
+            face.setImage("ecstatic");
+        }
+        else{
+            face.setMessage("1, 2, 3, jump.  Whew.");
+            face.setImage("tired");
+        }
     }
     
     public void sleep() {
@@ -37,3 +45,4 @@ public class VirtualPet {
     }
 
 } // end Virtual Pet
+ 
