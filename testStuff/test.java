e578858d9b695dd87a1be1ce21e61d01bc54d7f0
ari-r-3 <ari-r-3@ahs.local>
2023-10-11 15:00:54 -0700

    s

---
+++ testStuff/test.java
@@ -0,0 +1,20 @@
+package testStuff;
+
+import javax.swing.*;
+
+public class test {
+public static void main(String[] args) {
+    Object[] options = {
+        "Yes",
+        "No"};
+    int n = JOptionPane.showOptionDialog(new JFrame(),
+    "Is the world turning?",
+    "Q1",
+    JOptionPane.YES_NO_OPTION,
+    JOptionPane.QUESTION_MESSAGE,
+    null,
+    options,
+    options[1]);
+    System.out.println(n);
+    }
+}
